8f3ffd24108851cab72d6026aeb3c636568599b4
Simon <simon.chang@isobar.com>
2020-09-04 17:55:24 +0800

    Complete puzzle初始狀態

--- js/game.js
+++ js/game.js
@@ -1,11 +1,18 @@
 $(function () {
-  var puzzleItem = $(".puzzle-item");
+  var choosedQuestion = 0;
+  // var puzzleItem = $(".puzzle-item");
 
-  puzzleItem.on("click", function () {
-    var num = $(this).index() + 1;
-    var questionArea = $(".questionArea");
-    var questionWrap = $(".questionWrap--q" + num);
-    questionWrap.fadeIn();
-    questionArea.fadeIn();
+  // puzzleItem.on("click", function () {
+  //   var num = $(this).index() + 1;
+  //   var questionArea = $(".questionArea");
+  //   var questionWrap = $(".questionWrap--q" + num);
+  //   questionWrap.fadeIn();
+  //   questionArea.fadeIn();
+  // });
+
+  $('.puzzle-item').on('click', function () {
+    choosedQuestion = $(this).index() + 1;
+    $('.questionWrap--q' + choosedQuestion).fadeIn();
+    $('.questionArea').fadeIn();
   });
 });
